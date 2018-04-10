738f26debc29361dde8de3af31cd9d2af5d6485c
Woojoo Byun <woojoobyun@Woojoos-MacBook-Pro.local>
2018-04-10 14:42:48 -0400

    plz work

--- client/src/components/Results.js
+++ client/src/components/Results.js
@@ -2,19 +2,20 @@ import React from "react";
 
 const Results = props => (
     <div>
- 
+
         {props.results.map(result => (
             <div className="list-group-item" key={result.id}>
                 <img alt={result.name.last} className="img-responsive" src={result.picture.thumbnail} />
-           
+
             <h2> {result.name.first} {result.name.last} </h2>
             <p> {result.email} </p>
             <p> {result.location.postcode} </p>
+            <button onClick={props.onClick(result)}>Keep</button>
             </div>
-            
+
     ))}
 
-  </div>      
+  </div>
 );
 
 export default Results;
